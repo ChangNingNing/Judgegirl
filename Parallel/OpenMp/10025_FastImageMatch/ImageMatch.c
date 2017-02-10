1a5f4063b80d11aeb143464f45e6887a97a5e87d
ChangNingNing <r05922087@ntu.edu.tw>
2017-02-10 15:21:30 +0800

    10025_FastImageMatch

---
+++ Parallel/OpenMp/10025_FastImageMatch/ImageMatch.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <limits.h>
+
+#define MAXN 512
+short A[MAXN][MAXN];
+int B[MAXN][MAXN];
+int C[MAXN][MAXN];
+
+int main(){
+	int Ah, Aw, Bh, Bw;
+	while(scanf("%d %d %d %d", &Ah, &Aw, &Bh, &Bw)==4){
+		for(int i=0; i<Ah; i++)
+			for(int j=0; j<Aw; j++)
+				scanf("%d", &A[i][j]);
+
+		for(int i=0; i<Bh; i++)
+			for(int j=0; j<Bw; j++)
+				scanf("%d", &B[i][j]);
+
+		int min = INT_MAX, minX, minY;
+		#pragma omp parallel for
+		for(int i=0; i<=Ah-Bh; i++)
+			for(int j=0; j<=Aw-Bw; j++){
+				int sum = 0;
+				for(int k=0; k<Bh; k++){
+					for(int l=0; l<Bw; l++){
+						int tmp = A[i+k][j+l]-B[k][l];
+						sum += tmp * tmp;
+					}
+				}
+				C[i][j] = sum;
+				#pragma omp critical
+				if(C[i][j]<min){
+					min = C[i][j], minX = i, minY = j;
+				}
+
+			}
+		printf("%d %d\n", minX+1, minY+1);				
+	}
+	return 0;
+}
