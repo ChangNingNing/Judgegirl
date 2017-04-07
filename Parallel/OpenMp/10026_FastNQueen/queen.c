d8a2300af47ce604acaccff8508478d7b1a076b3
ChangNingNing <r05922087@ntu.edu.tw>
2017-04-07 22:06:36 +0800

    10026_FastNQueen use BFS and parallelized

--- Parallel/OpenMp/10026_FastNQueen/queen.c
+++ Parallel/OpenMp/10026_FastNQueen/queen.c
@@ -3,46 +3,80 @@
 #include <omp.h>
 #include <assert.h>
 #define MAXN 20
+#define MAXQ (1<<16)
 
 int isOccu[MAXN];
 
-int queen(int row, int d1, int d2, int next, int n){
+typedef struct {
+	int row, d1, d2, next;
+} Node;
+Node node[2][MAXQ];
+
+int n, c = 0;
+
+int queen (int row, int d1, int d2, int next){
     if(next >= n) return 1;
     int sum = 0;
-    int mask = isOccu[next] | row | d1 | d2;
-    mask = ~mask;
-    mask &= (1<<n)-1;
-    while(mask){
-        int p = mask & (-mask);
-        mask ^= p;
-        sum += queen(row|p, (d1|p)<<1, (d2|p)>>1, next+1, n);
-    }
+	int mask = isOccu[next] | row | d1 | d2;
+	mask = ~mask;
+	mask &= (1<<n)-1;
+	while(mask){
+		int p = mask & (-mask);
+		mask ^= p;
+		sum += queen(row|p, (d1|p)<<1, (d2|p)>>1, next+1);
+	}
     return sum;
 }
 
+int BFS(){
+	int nSol = 0;
+	int pre_l, pre_r, cur_l = 0, cur_r = 1;
+	Node *pre_b = node[1], *pre_p, *cur_b = node[0], *cur_p;
+	while (cur_r * (n-3) < MAXQ && cur_r != 0){
+		pre_l = cur_l, pre_r = cur_r, cur_l = cur_r = 0;
+		{ Node *t = pre_b; pre_b = cur_b; cur_b = t; } // Swap
+		pre_p = pre_b, cur_p = cur_b;
+		while (pre_l < pre_r){
+			if (pre_p->next >= n){
+				nSol++, pre_l++, pre_p++;
+				continue;
+			}
+			int row = pre_p->row, d1 = pre_p->d1, d2 = pre_p->d2, next = pre_p->next;
+			int mask = isOccu[next] | row | d1 | d2;
+			mask = ~mask;
+			mask &= (1 << n) - 1;
+			while (mask){
+				int p = mask & (-mask);
+				mask ^= p;
+				cur_p->row = row | p;
+				cur_p->d1 = (d1 | p) << 1;
+				cur_p->d2 = (d2 | p) >> 1;
+				cur_p->next = next + 1;
+				cur_p++, cur_r++;
+			}
+			pre_l++, pre_p++;
+		}
+	}
+
+	#pragma omp parallel for schedule(dynamic) reduction(+ : nSol)
+	for(int i=cur_l; i<cur_r; i++)
+		nSol += queen(cur_b[i].row, cur_b[i].d1, cur_b[i].d2, cur_b[i].next);
+	return nSol;
+}
+
 int main(){
-    int n, c = 0;
-    while(scanf("%d", &n)==1){
+    while (scanf("%d", &n)==1){
         c++;
-        char tmp[32];
-        for(int i=0; i<n; i++){
-            isOccu[i] = 0;
+		char tmp[32];
+        for (int i=0; i<n; i++){
+			isOccu[i] = 0;
             scanf("%s", tmp);
-            for(int j=0; j<n; j++)
-                if(tmp[j]=='*')
-                    isOccu[i] |= (1<<j);
-        }
-        int nSol = 0;
-        #pragma omp parallel for schedule(static, 1) reduction(+ : nSol)
-        for(int i=0; i<n; i++){
-            if(!((isOccu[0]>>i) & 1)){
-                int pos[MAXN];
-                int row=0, d1=0, d2=0;
-                pos[0] = i;
-                d1 = d2 = row |= (1<<i);
-                nSol += queen(row, d1<<1, d2>>1, 1, n);
-            }
-        }
+			for(int j=0; j<n; j++)
+				if(tmp[j]=='*')
+					isOccu[i] |= (1<<j);
+		}
+		node[0][0].next = node[0][0].row = node[0][0].d1 = node[0][0].d2 = 0;
+        int nSol = BFS();
         printf("Case %d: %d\n", c, nSol);
     }
     return 0;
