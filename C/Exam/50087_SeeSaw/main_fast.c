f1f89bcefe83f44f3795045814a267a2a69b6cf2
ChangNingNing <r05922087@ntu.edu.tw>
2017-11-27 13:22:26 +0800

    50087 Seesaw with O(n) solution

--- C/Exam/50087_SeeSaw/main_fast.c
+++ C/Exam/50087_SeeSaw/main_fast.c
@@ -2,7 +2,6 @@
 #define MAXN 2048
 
 int W[MAXN];
-int S[MAXN];
 
 int main(){
 	int N;
@@ -10,57 +9,27 @@ int main(){
 	for (int i=0; i<N; i++)
 		scanf("%d", &W[i]);
 
+	int sum = 0, w = 0;
 	for (int i=0; i<N; i++){
-		int cur = W[i];
-		for (int j=i-1; j>=0; j--){
-			S[j] += cur;
-			cur += W[i];
-		}
-		cur = W[i];
-		for (int j=i+1; j<N; j++){
-			S[j] -= cur;
-			cur += W[i];
-		}
+		sum += W[i];
+		w += W[i] * i;
 	}
 
 	int sup = -1;
-	int noAns = 1;
-	for (int swap = 0; swap < N/2+1 && noAns; swap++){
-		/*
-		for (int i=1; i<N-1 && noAns; i++){
-			if (S[i] == 0){
-				sup = i;
-				noAns = 0;
-			}
+	for (int swap = 0; swap < N/2+1; swap++){
+		if (w % sum == 0){
+			sup = w / sum;
+			break;
 		}
-		*/
-		{
-			int l = 1, r = N-2;
-			while (l <= r){
-				int mid = (l + r) / 2;
-				if (S[mid] <= 0) r = mid - 1;
-				else l = mid + 1;
-			}
-			if (S[l] == 0){
-				sup = l;
-				noAns = 0;
-			}
-		}
-
-		if (noAns){
+		else {
 			int _swap = N-1-swap;
-			for (int i=0; i<N; i++){
-				if (i < swap)
-					S[i] += W[swap]*(_swap-swap) + W[_swap]*(swap-_swap);
-				else if (i == swap)
-					S[i] += W[swap]*(_swap-i) - W[_swap]*(_swap-i);
-				else
-					S[i] += W[swap]*(_swap-swap) - W[_swap]*(_swap-swap);
-			}
+			w -= W[swap] * swap + W[_swap] * _swap;
 
 			int tmp = W[swap];
 			W[swap] = W[_swap];
 			W[_swap] = tmp;
+
+			w += W[swap] * swap + W[_swap] * _swap;
 		}
 	}
 
